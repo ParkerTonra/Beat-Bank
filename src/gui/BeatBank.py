a039bba3dab02ca74b93e80ceff53f37ca523431
J Parker Tonra <parkertonra@gmail.com>
2023-12-02 15:31:47 -0700

    menu bar implemented

--- src/gui/BeatBank.py
+++ src/gui/BeatBank.py
@@ -1,4 +1,6 @@
 import datetime
+import platform
+import subprocess
 import mutagen, os, time, sys
 
 from PyQt6 import QtCore, QtGui, QtWidgets
@@ -8,17 +10,15 @@ from PyQt6.QtWidgets import (
     QWidget, QLabel, QFileDialog, QHeaderView, QMessageBox, QHBoxLayout, QFrame,
     QPushButton, QAbstractItemView, QTableView, QAbstractScrollArea, QMenu
 )
-from PyQt6.QtCore import Qt, QUrl, QMimeData, QTime, QEvent, QDateTime, pyqtSignal
+from PyQt6.QtCore import Qt, QUrl, QMimeData, QTime, QEvent, QDateTime, pyqtSignal, QSettings
 # import pyqt6 QAction
 from PyQt6.QtGui import QAction
 from controllers.database_controller import DatabaseManager
 from gui import event_handlers
 from gui.edit_track_window import EditTrackWindow
-#TODO: get rid of buttons and place in toolbar
+#TODO: Figure out way column's won't resize, figure out why open file location isn't working
 #TODO: user stories:
 # user story: user can right click a track and select "open file location" to open the folder where the track is located
-# user story: user can right click a track and select "edit" to open the edit track window
-# user story: user can right click a track and select "delete" to delete the track from the database
 # user story: there is a group of items on the context menu called "show similar" when the user highlights this,
     # an option comes up to show similar tracks by bpm or key (eventually tags perhaps) in the filtered table view
 # user story: user can right click a track and select "open in ableton" to open the ableton project associated with the track :O
@@ -26,6 +26,12 @@ from gui.edit_track_window import EditTrackWindow
 # user story: user can right click a track and select "play audio" to play the audio file in the default audio player
 # user story: user can go to settings from the toolbar and "enable/disable" edit mode to reorganize the table
 # User story : user can drag and drop a file from this program to another
+# user story: users can change the width of the columns, and the changes are saved
+    #user story: users can lock column width from settings menu
+    
+#COMPLETED:
+# user story: users can change the order of the columns, and the changes are saved
+    #user story: users can lock column order from settings menu
 '''# old imports
 from database import SessionLocal, init_db
 from business.track_business_model import TrackBusinessModel
@@ -43,6 +49,9 @@ from controllers.track_controller import TrackController'''
 # BeatBank.py (root)/src/gui/BeatBank.py
 
 class MainWindow(QMainWindow):
+    # -------------------------------------------------------------------------
+    # Initialization / UI Setup
+    # -------------------------------------------------------------------------
     def __init__(self):
         super().__init__()
         self.controller = DatabaseManager(self)
@@ -51,15 +60,15 @@ class MainWindow(QMainWindow):
         self.init_ui()
         self.table.trackDropped.connect(self.add_track)
         self.editWindow = None
-        
 
     def init_ui(self):
         self.setupWindow()
         self.init_setupLayouts()
         self.init_beat_model()
         self.init_beat_table()
+        self.restore_table_state()
         self.init_filteredTableView() #TODO
-        self.setupButtons()
+        # self.setupButtons()
         self.init_menu_bar()
         self.finalizeLayout()
         # Populate the model
@@ -71,6 +80,16 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 1400, 200)
         print("Window set up")
 
+    def finalizeLayout(self):
+        print("Finalizing layout...")
+        self.container = QWidget(self)
+        self.container.setLayout(self.main_layout)
+        self.setCentralWidget(self.container)
+        self.table_layout.addWidget(self.table)
+    
+    # -------------------------------------------------------------------------
+    # UI component initialization
+    # -------------------------------------------------------------------------
     def init_setupLayouts(self):
         print("Setting up layouts...")
         self.main_layout = QVBoxLayout()
@@ -97,12 +116,21 @@ class MainWindow(QMainWindow):
         self.model.setTable('tracks')
         self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnFieldChange)
         self.model.select()
+        
+        
         print("Model initialized")
 
     def init_beat_table(self):
         print("Initializing table...")
         self.table = BeatTable(self)
         self.table.setModel(self.model)
+        # Hide unnecessary columns
+        self.table.hideColumn(0)  
+        self.table.hideColumn(8)  
+        self.table.hideColumn(10) 
+        #refresh
+        self.table_refresh()
+        
         print("Table initialized")
 
     def init_filteredTableView(self):
@@ -114,93 +142,130 @@ class MainWindow(QMainWindow):
         # users can select which columns to display
         #users can toggle this table on and off
         #users can select which columns to display  
-        
-    def setupButtons(self):
-        # Refresh button
-        self.refresh_button = QPushButton('Refresh', self)
-        self.refresh_button.clicked.connect(self.table_refresh)
-        self.refresh_button.setIcon(QtGui.QIcon('src/assets/pictures/refresh.png'))
-        # 'Add Track' Button
-        self.add_button = QPushButton('Add track', self)
-        self.add_button.clicked.connect(self.add_track)
-        # 'Delete Track' Button
-        self.delete_button = QPushButton('Delete track', self)
-        self.delete_button.clicked.connect(self.delete_track)
-        # 'Edit Track' Button
-        self.edit_button = QPushButton('Edit track', self)
-        self.edit_button.clicked.connect(self.edit_track)
-        # 'Settings' Button
-        self.settings_button = QPushButton('Settings', self)
-        self.settings_button.clicked.connect(self.open_settings)
-        
-        # Add widgets to the right/left layouts
-        self.left_layout.addWidget(self.refresh_button)
-        # TODO: left_layout.addWidget(self.filteredTableView)
-        self.right_layout.addWidget(self.add_button)
-        self.right_layout.addWidget(self.delete_button)
-        self.right_layout.addWidget(self.edit_button)
-        self.right_layout.addWidget(self.edit_button)
-
-    def finalizeLayout(self):
-        print("Finalizing layout...")
-        self.container = QWidget(self)
-        self.container.setLayout(self.main_layout)
-        self.setCentralWidget(self.container)
-        self.table_layout.addWidget(self.table)
-        
-
     
-    # ORDER THIS LATER
+    # -------------------------------------------------------------------------
+    # Menu bar and Actions
+    # -------------------------------------------------------------------------
     def init_menu_bar(self):
         # Create Menu Bar
         menu_bar = self.menuBar()
-        
+
         # Add Menus
         file_menu = menu_bar.addMenu("&File")
         edit_menu = menu_bar.addMenu("&Edit")
         view_menu = menu_bar.addMenu("&View")
         settings_menu = menu_bar.addMenu("&Settings")
         help_menu = menu_bar.addMenu("&Help")
-        
+        columns_menu = view_menu.addMenu("&Columns")
+        self.init_columns_submenu(columns_menu)
 
-
-    # Add Actions - delete , refresh
-        # file menu actions
+        # File Menu Actions
         exit_action = QAction("&Exit", self)
-        add_track_action = QAction("&Add Track", self)
-        refresh_action = QAction("&Refresh", self)
-        
-        # edit menu actions
-        edit_track_action = QAction("&Edit Track", self)
-        edit_test_action = QAction("&Edit Test", self)
-        delete_track_action = QAction("&Delete Track", self)
-        
-        
-        #view menu actions
-            #TODO - lock / unlock the sorting of the table. - sort by different columns
-            #TODO: enable / disable columns
-        
-        # connect the actions 
         exit_action.triggered.connect(sys.exit)
-        add_track_action.triggered.connect(self.add_track)
-        edit_track_action.triggered.connect(self.edit_track)
-        delete_track_action.triggered.connect(self.delete_track)
         
-        file_menu.addAction(exit_action)
+        add_track_action = QAction("&Add Track", self)
+        add_track_action.triggered.connect(self.add_track)
+        
+        refresh_action = QAction("&Refresh", self)
+        refresh_action.triggered.connect(self.table_refresh)
+
         file_menu.addAction(add_track_action)
         file_menu.addAction(refresh_action)
+        file_menu.addAction(exit_action)
+
+        # Edit Menu Actions
+        edit_track_action = QAction("&Edit Track", self)
+        edit_track_action.triggered.connect(self.edit_track)
         
+        delete_track_action = QAction("&Delete Track", self)
+        delete_track_action.triggered.connect(self.delete_track)
+
         edit_menu.addAction(edit_track_action)
-        edit_menu.addAction(edit_test_action)
         edit_menu.addAction(delete_track_action)
         
+        # Settings Menu Actions
+        self.toggle_reorder_action = QAction("&Allow reorder", self, checkable=True)
+        self.toggle_reorder_action.triggered.connect(self.toggle_reorder)
+        
+        settings_menu.addAction(self.toggle_reorder_action)
 
-# =============================================================================
-# MAIN WINDOW FUNCTIONS
-# =============================================================================    
+        
 
+        # View Menu Actions
+        self.show_similar_tracks_action = QAction("Always Show Similar Tracks Table", self, checkable=True)
+        self.show_similar_tracks_action.toggled.connect(self.toggle_similar_tracks)
+        
+        
+        view_menu.addAction(self.show_similar_tracks_action)
+
+        # Help Menu Actions
+        read_me_action = QAction("Read Me", self)
+        # Connect read_me_action to the appropriate slot
+        help_menu.addAction(read_me_action)
+
+    def init_columns_submenu(self, columns_menu):
+        for i in range(self.table.model().columnCount()):
+            column_name = self.table.model().headerData(i, Qt.Orientation.Horizontal)
+            action = QAction(column_name, self, checkable=True)
+            settings = QSettings("Parker Tonra", "Beat Bank")
+            action.setChecked(not self.table.isColumnHidden(i))
+            action.toggled.connect(lambda checked, index=i: self.toggle_column(checked, index))
+            columns_menu.addAction(action)
+
+    def toggle_column(self, checked, index):
+        self.table.setColumnHidden(index, not checked)
+        settings = QSettings("Parker Tonra", "Beat Bank")
+        settings.setValue(f"columnVisibility/{index}", checked)
+
+    def toggle_similar_tracks(self, checked):
+        # Emit signal or perform action based on the state of the toggle
+        if checked:
+            # Emit signal or perform action to show similar tracks table
+            pass
+        else:
+            # Emit signal or perform action to hide similar tracks table
+            pass
+    
+    def toggle_reorder(self, checked):
+        # Emit signal or perform action based on the state of the toggle
+        if checked:
+            # Emit signal or perform action to allow reordering
+            pass
+        else:
+            # Emit signal or perform action to disallow reordering
+            pass
+
+    def open_read_me(self):
+        # Open the readme file
+        pass
+    
     # -------------------------------------------------------------------------
-    # FUNCTIONS LINKED TO BUTTONS
+    # Context Menu
+    # -------------------------------------------------------------------------
+    def contextMenuEvent(self, event):
+        context_menu = QMenu(self)
+        open_file_action = context_menu.addAction("Open File Location")
+        edit_track_action = context_menu.addAction("Edit Track")
+        delete_track_action = context_menu.addAction("Delete Track")
+        open_ableton_action = context_menu.addAction("Open in Ableton") #TODO
+        add_to_set_action = context_menu.addAction("Add to Set") #TODO
+        play_audio_action = context_menu.addAction("Play Audio") #TODO
+        action = context_menu.exec(event.globalPos())
+        if action == open_file_action:
+            self.open_file_location()
+        elif action == edit_track_action:
+            self.edit_track()
+        elif action == delete_track_action:
+            self.delete_track()
+        elif action == open_ableton_action:
+            pass
+        elif action == add_to_set_action:
+            pass
+        elif action == play_audio_action:
+            pass
+    
+    # -------------------------------------------------------------------------
+    # Button linked functions
     # -------------------------------------------------------------------------
     def add_track(self, path=None):
         print("Adding a new track to the database...")
@@ -244,6 +309,17 @@ class MainWindow(QMainWindow):
         else:
             print("Failed to add track:", query.lastError().text())
     
+    def edit_track(self):
+        print("Editing track...")
+        selected_row = self.table.currentIndex().row()
+        if selected_row < 0:
+            print("No track selected for editing.")
+            return
+        track_id = self.model.record(selected_row).value("id")
+        self.editWindow = EditTrackWindow(track_id)
+        self.editWindow.trackEdited.connect(self.table_refresh)
+        self.editWindow.show()
+    
     def delete_track(self):
         selected_row = self.table.currentIndex().row()
         if selected_row < 0:
@@ -261,7 +337,33 @@ class MainWindow(QMainWindow):
         else:
             print("Track deletion cancelled.")
             return
+    
+    def open_file_location(self):
+        current_row = self.table.currentIndex().row()
+        if current_row < 0:
+            self.show_warning_message("No Selection", "Please select a track to open its file location.")
+            return
 
+        file_path = self.model.record(current_row).value("file_path")
+        print(f"Opening file location for {file_path}")
+        if not os.path.exists(file_path):
+            self.show_warning_message("File Not Found", "The file path does not exist.")
+            return
+
+        if platform.system() == 'Windows':
+            print("Windows OS detected. Opening file location...")
+            subprocess.Popen(f'explorer /select,"{file_path}"', shell=True)
+        elif platform.system() == 'Darwin':  # macOS
+            print("macOS detected. Opening file location...")
+            subprocess.Popen(['open', '-R', file_path])
+        else:  # Assuming Linux or other Unix-like OS
+            print("Linux or Unix-like OS detected. Opening file location...")
+            subprocess.Popen(['xdg-open', file_path])
+        
+    
+    # -------------------------------------------------------------------------
+    # Database operations
+    # -------------------------------------------------------------------------
     def delete_from_database(self, track_id):
         query = QSqlQuery()
         query.prepare("DELETE FROM tracks WHERE id = :id")
@@ -272,7 +374,7 @@ class MainWindow(QMainWindow):
         else:
             print("Failed to delete track:", query.lastError().text())
 
-
+    # Utility functions
     def show_warning_message(self, title, message):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Icon.Warning)
@@ -288,28 +390,6 @@ class MainWindow(QMainWindow):
     def handle_update_error(self, exception):
         # Log the error, show a message box, or take other appropriate action
         print(f"An error occurred during table update: {exception}")
-       
-
-    
-    def edit_track(self):
-        print("Editing track...")
-        selected_row = self.table.currentIndex().row()
-        if selected_row < 0:
-            print("No track selected for editing.")
-            return
-        track_id = self.model.record(selected_row).value("id")
-        self.editWindow = EditTrackWindow(track_id)
-        self.editWindow.trackEdited.connect(self.table_refresh)
-        self.editWindow.show()
-        
-
-    def open_settings(self):
-        # open the settings window
-        pass
-    
-    # -----------------------------------------------------------------------------
-    # FUNCTIONS LINKED TO TABLE
-    # -----------------------------------------------------------------------------
     
     def table_refresh(self):
         print("Refreshing table...")
@@ -317,10 +397,24 @@ class MainWindow(QMainWindow):
         self.model.select()
         pass
     
-    def update_table_row(self, track_id):
-        # Update a row in the table
-        pass
-    
+    def restore_table_state(self):
+        print("Restoring table state...")
+        settings = QSettings("Parker Tonra", "Beat Bank")
+        state = settings.value("tableState")
+        if state:
+            print(f"State found. Restoring table state...")
+            self.table.horizontalHeader().restoreState(state)
+            for i in range(self.table.model().columnCount()):
+                visible = settings.value(f"columnVisibility/{i}", True, type=bool)
+                self.table.setColumnHidden(i, not visible)
+        else:
+            print("No state found.")          
+    def closeEvent(self, event):
+        print("Shutting down...")
+        settings = QSettings("Parker Tonra", "Beat Bank")
+        settings.setValue("tableState", self.table.horizontalHeader().saveState())
+        super().closeEvent(event)
+
 
 class BeatTable(QTableView):
     trackDropped = pyqtSignal(str)
@@ -332,8 +426,9 @@ class BeatTable(QTableView):
         self.setAcceptDrops(True)
         self.setDragEnabled(True)
         self.setMinimumHeight(500)
-        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
-    
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Interactive)
+        self.horizontalHeader().setSectionsMovable(True)
+
     
     def handleSingleClick(self, event):
         event_handlers.handleSingleClick(self, event)
@@ -347,10 +442,3 @@ class BeatTable(QTableView):
         event_handlers.dragMoveEvent(self, event)
     def dropEvent(self, event):
         event_handlers.dropEvent(self, event)
-    
-    
-    #old
-    '''def handleDoubleClick(self, event):
-        event_handlers.handleDoubleClick(self, event)
-    def mousePressEvent(self, event):
-        event_handlers.tableMousePressEvent(self, event)'''
